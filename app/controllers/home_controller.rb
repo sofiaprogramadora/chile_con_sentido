c6e19748338e4277b2398ae25879454574a6f4bc
Gonzalo Salinas <gsalinase91@gmail.com>
2017-08-27 04:19:53 -0300

    Cargando código twitter

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,13 +9,6 @@ class HomeController < ApplicationController
       config.access_token_secret = 'XQZTbw2mdzl63B09pjEeTosLBxUR2NBaynl3j5paK78jf'
     end
 
-    @client_2 = Twitter::Streaming::Client.new do |config|
-      config.consumer_key        = 'OXhoB33kWBAOSBpFg8M1f5iRu'
-      config.consumer_secret     = 'SPCl3qvrPAfx1lUh6Mf8L0r0uknvDOiSf7XSbznCD1s6UPoUV5'
-      config.access_token        = '901604568148168706-Z7R1ffWTFZqQNWHVA3DxJFa1J76Lpn6'
-      config.access_token_secret = 'XQZTbw2mdzl63B09pjEeTosLBxUR2NBaynl3j5paK78jf'
-    end
-
   end
 
   def result
