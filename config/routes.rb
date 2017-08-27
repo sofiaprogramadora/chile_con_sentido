791da20974a167755402f6774a2652fc2875f671
Guillermo <gibsonguillermoarturo@gmail.com>
2017-08-27 00:40:20 -0300

    Configuración de Twitter

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   get 'home/result'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#main'
+  
 end
