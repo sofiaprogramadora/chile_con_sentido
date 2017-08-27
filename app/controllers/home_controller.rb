c3395438571ba73de18a522f22daa0c7b78f1add
Sofía Castillo <sofiaprogramadora@gmail.com>
2017-08-27 07:01:54 -0300

    Final commit! From the engine...

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,17 @@
 class HomeController < ApplicationController
-  
+
   def main
+    @var = params[:search]
+  end
 
 
-
-
-
+  def show_result
+    @var = params[:search]
+    redirect_to home_result_path(search: @var), notice: "Su busqueda no obtuvo resultados"
   end
 
   def result
-
+    @var = params[:search]
     @client = Twitter::REST::Client.new do |config|
       config.consumer_key        = 'OXhoB33kWBAOSBpFg8M1f5iRu'
       config.consumer_secret     = 'SPCl3qvrPAfx1lUh6Mf8L0r0uknvDOiSf7XSbznCD1s6UPoUV5'
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get 'home/main'
 
   get 'home/result'
-
+  post 'home/result'
   root 'home#main'
+
+  post 'home/show_result'
+
 end
