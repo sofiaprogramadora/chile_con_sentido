7fbc62696d6cf62c5d9b0925e50481065b938952
Sofía Castillo <sofiaprogramadora@gmail.com>
2017-08-27 00:56:16 -0300

    Finish home page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   get 'home/result'
 
+  root 'home#main'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
