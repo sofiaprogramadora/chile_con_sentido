0920c5fc6de7bd96d18d59bfdd4032f691dd39ca
Guillermo <gibsonguillermoarturo@gmail.com>
2017-08-30 20:09:35 -0300

    Cambios

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,10 +11,7 @@ class HomeController < ApplicationController
 
     @var = params[:search]
     @client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = 'OXhoB33kWBAOSBpFg8M1f5iRu'
-      config.consumer_secret     = 'SPCl3qvrPAfx1lUh6Mf8L0r0uknvDOiSf7XSbznCD1s6UPoUV5'
-      config.access_token        = '901604568148168706-Z7R1ffWTFZqQNWHVA3DxJFa1J76Lpn6'
-      config.access_token_secret = 'XQZTbw2mdzl63B09pjEeTosLBxUR2NBaynl3j5paK78jf'
+
     end
 
     @client_query = @client.search("to:#{@var}", result_type: "recent")
